90f67b4cdb4909550352ff689da8c8a631d0588a
mason <wukaixiang331@163.com>
2025-04-21 01:00:22 +0800

    prefix

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,7 +6,7 @@ import { defineNoteConfig,plumeTheme } from 'vuepress-theme-plume'
 
 
 export default defineUserConfig({
-  base: "/<REPO>/",
+  base: "/zwds/",
   lang: 'zh-CN',
   locales: {
     '/': {
